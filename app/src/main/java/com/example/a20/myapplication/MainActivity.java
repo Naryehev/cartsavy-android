bc86baf0b8bd766206249a6611e25fd0affeb972
Unknown <daniel.naryshev@gmail.com>
2017-07-09 23:19:25 -0700

    somthing from home
    idk tbh

--- app/src/main/java/com/example/a20/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a20/myapplication/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
